51fb2c130aa9c4401a5f17b2cc8fafd1bcf06f43
MatiasFGarciaDox <mfernandez8@estudiante.ceibal.edu.uy>
2024-07-19 15:14:07 -0300

    Prograso en alta y modificacion

---
+++ Controladores/ControlPersona.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using Modelos;
+
+namespace Controladores
+{
+    public class ControlPersona
+    {
+        public static void CrearPersona(string id,string nombre,string apellido)
+        {
+            ModeloPersona persona = new ModeloPersona();
+            persona.Id = id;
+            persona.Nombre = nombre;
+            persona.Apellido = apellido;
+
+            persona.GuardarPersona();
+        }
+
+        public static void ModificarNomPersona(string id, string nombre)
+        {
+            ModeloPersona persona = new ModeloPersona();
+            persona.Id = id;
+            persona.Nombre = nombre;
+            persona.ModificarNombrePersona();
+        }
+
+        public static void ModificarApePersona(string id,string apellido)
+        {
+            ModeloPersona persona = new ModeloPersona();
+            persona.Id = id;
+            persona.Apellido = apellido;
+            persona.ModificarApellidoPersona();
+        }
+
+        public static DataTable Listar()
+        {
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Id", typeof(string));
+            tabla.Columns.Add("Nombre", typeof(string));
+            tabla.Columns.Add("Apellido", typeof(string));
+
+            ModeloPersona persona = new ModeloPersona();
+            foreach (ModeloPersona p in persona.ObtenerPersonas())
+            {
+                DataRow fila = tabla.NewRow();
+                fila["Id"] = p.Id;
+                fila["Nombre"] = p.Nombre;
+                fila["Apellido"] = p.Apellido;
+
+                tabla.Rows.Add(fila);
+            }
+            return tabla;
+        }
+    }
+}
--- Modelos/ModeloPersona.cs
+++ Modelos/ModeloPersona.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Modelos
 {
-    class ModeloPersona : Modelo
+    public class ModeloPersona : Modelo
     {
         public string Id;
         public string Nombre;
